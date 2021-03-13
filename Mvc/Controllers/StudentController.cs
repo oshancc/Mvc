91825503669ebf03032077502ba2911b0add0523
Oshan <oshancc@gmail.com>
2021-03-13 11:40:42 +0530

    Edit() created

--- Mvc/Controllers/StudentController.cs
+++ Mvc/Controllers/StudentController.cs
@@ -41,5 +41,35 @@ namespace Mvc.Controllers
             return RedirectToAction("Index", new { id = objStudent.CourseId });
 
         }
+
+        public IActionResult Edit(int id=0)
+        {
+            var student = db.Students.Find(id);
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "CourseName", student.CourseId);
+            if(student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+
+        }
+
+        private IActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student objStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(objStudent).State = EntityState.Modified;
+               
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index", new { id = objStudent.CourseId });
+
+        }
     }
 }
